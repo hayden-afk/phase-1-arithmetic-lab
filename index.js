5777d7d61080b3f96f690d15ae9b7ce321752dd7
Hayden Smith <hayden.smith121896@gmail.com>
2022-02-01 13:26:01 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+
+var random = Math.floor(Math.random() * 10 ) +1;
+
+
+let num3 = 16
+let num4 = 4
+
+const mod = num3 / num4
+
+
+const max = Math.max(1,2,3,4,20)
